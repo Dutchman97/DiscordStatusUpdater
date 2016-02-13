0831a277c3c22ccbb6afa0de63c9d9b51e4cb933
Dutchman97 <makatipu97@hotmail.com>
2016-02-14 00:30:29 +0100

    Discord.Net (v0.9.0 rc3) now nicely included through NuGet

--- DiscordStatusUpdater/DiscordStatusUpdater/Form1.cs
+++ DiscordStatusUpdater/DiscordStatusUpdater/Form1.cs
@@ -1,19 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
+using System.IO;
 using System.Windows.Forms;
+using Discord;
 
 namespace DiscordStatusUpdater
 {
     public partial class Form1 : Form
     {
+        DiscordClient client;
+        string email, password;
+
         public Form1()
         {
             InitializeComponent();
+
+            string[] loginInfo = File.ReadAllLines(@"C:\Users\Kevin\Desktop\login.txt");
+            email = loginInfo[0]; password = loginInfo[1];
+
+            client = new DiscordClient();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            client.Disconnect();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            client.Connect(email, password);
         }
     }
 }
